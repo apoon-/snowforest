b95281626a659c1c914e8e88495188793477e23e
apoon- <anson.os.poon@gmail.com>
2013-11-09 22:32:06 -0500

    added sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,39 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user_id
+      redirect_to products_url, :notice => "Logged in!"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to products_url, :notice => "Logged Out!"
+  end
+end
+
+# class SessionsController < ApplicationController
+#   def new
+#   end
+
+#   def create
+#     user = User.find_by_email(params[:email])
+#     if user && user.authenticate(params[:password])
+#       session[:user_id] = user.id
+#       redirect_to products_url, :notice => "Logged in!"
+#     else
+#       render "new"
+#     end
+#   end
+
+#   def destroy
+#     session[:user_id] = nil
+#     redirect_to products_url, :notice => "Logged out!"
+#   end
+# end
